8b701b246d009b899b480eafbbb67a8e209bf7f1
Mathias Bøe <mathias@islandgarden.no>
2021-09-22 16:18:41 +0200

    fixed index out of bounds issue in learn

--- testfil.py
+++ testfil.py
@@ -8,13 +8,13 @@ import time as t
 
 df = pd.read_csv("magic04.data")
 
-X = df.to_numpy()[:,0:10]
-Y = df.to_numpy()[:,10]
+X = df.to_numpy()[:, 0:10]
+Y = df.to_numpy()[:, 10]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.90)
 
 
-def getLabelSplitAvg(X : np.ndarray, y : list, col : int) -> Tuple[list, list]:
+def getLabelSplitAvg(X: np.ndarray, y: list, col: int) -> Tuple[list, list]:
     xAvg = np.sum(X[:][col])/len(y)
     aboveEq = []
     below = []
@@ -25,7 +25,8 @@ def getLabelSplitAvg(X : np.ndarray, y : list, col : int) -> Tuple[list, list]:
             below.append(y[i])
     return (aboveEq, below)
 
-def getLabelSplitAvgX(X : np.ndarray, y : list, col : int) -> Tuple[list, list]:
+
+def getLabelSplitAvgX(X: np.ndarray, y: list, col: int) -> Tuple[list, list]:
     xAvg = np.sum(X[:][col])/len(y)
     aboveEq = []
     below = []
@@ -41,39 +42,42 @@ def getLabelSplitAvgX(X : np.ndarray, y : list, col : int) -> Tuple[list, list]:
 def entropy(y):
     if len(y) == 0:
         return 0
-    probOfG = list(y).count('g')/(len(y)) 
+    probOfG = list(y).count('g')/(len(y))
     probOfH = 1 - probOfG
     if probOfG == 1 or probOfG == 0:
         return probOfG
     return (-1) * (probOfG * np.log2(probOfG) + (probOfH) * np.log2(probOfH))
 
-def entropySplit(X : List[list], y : list):
+
+def entropySplit(X: List[list], y: list):
     yEntropy = entropy(y)
     allEnt = []
     for col in range(len(X[0])):
         aboveEq, below = getLabelSplitAvg(X, y, col)
-        allEnt.append(yEntropy - entropy(aboveEq)*len(aboveEq)/len(y) + entropy(below)*len(aboveEq)/len(y))
+        allEnt.append(yEntropy - entropy(aboveEq)*len(aboveEq) /
+                      len(y) + entropy(below)*len(aboveEq)/len(y))
     return allEnt.index(max(allEnt))
-    
+
 
 def learn(X, y):
-    if len(set(y))==1:
+    if len(set(y))-1 == 1:
         return y[0]
 
-    elif all(list(map(lambda x: all(list(map(lambda y: X[0][0]==y, x))), list(X)))):
-        return 'g' if list(y).count('g')>(len(y)/2) else 'h'
-    
+    elif all(list(map(lambda x: all(list(map(lambda y: X[0][0] == y, x))), list(X)))):
+        return 'g' if list(y).count('g') > (len(y)-1/2) else 'h'
+
     else:
-        splitIndex = entropySplit(X,y)
+        splitIndex = entropySplit(X, y)
         x1, x2 = getLabelSplitAvgX(X, y, splitIndex)
         y1, y2 = getLabelSplitAvg(X, y, splitIndex)
 
-        return (learn(x1, y1), learn(x2, y2), splitIndex, np.sum(X[:, splitIndex])/len(y))
+        return (learn(x1, y1), learn(x2, y2), splitIndex, np.sum(X[:, splitIndex])/len(y)-1)
 
 # tree = learn(X, y)
 # predict(new_x, tree)
 
-t = learn(X_test,Y_test)
+
+t = learn(X_test, Y_test)
 
 
 def predict(x, tree):
@@ -87,4 +91,5 @@ def predict(x, tree):
 #g = learn(X_train, Y_train)
 #print(predict([36.1741,17.6865,2.946,0.2865,0.1591,-4.7746,-18.9697,11.3256,0.254,191.455], g))
 
-print(predict(X_train[0],t))
+
+print(predict(X_train[0], t))
